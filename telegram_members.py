5804227d237790f5c8851ba28240d7da85df95be
Abioye Bankole <abioye.bankole@vibes.com>
2021-05-30 12:00:03 +0100

    Better group management

--- telegram_members.py
+++ telegram_members.py
@@ -9,6 +9,39 @@ from collections import OrderedDict
 api_id = 3561678
 api_hash = '369f403b32609b76128abdf8453fc9ca'
 phone = '+2348127350135'
+
+def print_groups(groups):
+    for i in range(len(groups)):
+        print_group(groups[i])
+
+def print_group(target_group):
+  print('Fetching Members...')
+  all_participants = []
+  all_participants = client.get_participants(target_group, aggressive=True)
+
+  print('Saving In file...')
+  with open(target_group.title+"-"+date.today().isoformat()+".csv", "w", encoding='UTF-8') as f:
+      writer = csv.writer(f, delimiter=",", lineterminator="\n")
+      writer.writerow(['username', 'user id', 'access hash',
+                      'name', 'group', 'group id'])
+      for user in all_participants:
+          if user.username:
+              username = user.username
+          else:
+              username = ""
+          if user.first_name:
+              first_name = user.first_name
+          else:
+              first_name = ""
+          if user.last_name:
+              last_name = user.last_name
+          else:
+              last_name = ""
+          name = (first_name + ' ' + last_name).strip()
+          writer.writerow([username, user.id, user.access_hash,
+                          name, target_group.title, target_group.id])
+  print('Members scraped successfully.')
+
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
@@ -53,34 +86,13 @@ for g in groups:
     print(str(i) + '- ' + g.title)
     i += 1
 
-g_index = -2
-while g_index != -1:
+g_index = ''
+while True:
   g_index = input("Enter a Number from 0 to "+str(len(groups)-1) +": ")
-  target_group = groups[int(g_index)]
-
-  print('Fetching Members...')
-  all_participants = []
-  all_participants = client.get_participants(target_group, aggressive=True)
-
-  print('Saving In file...')
-  with open(target_group.title+"-"+date.today().isoformat()+".csv", "w", encoding='UTF-8') as f:
-      writer = csv.writer(f, delimiter=",", lineterminator="\n")
-      writer.writerow(['username', 'user id', 'access hash',
-                      'name', 'group', 'group id'])
-      for user in all_participants:
-          if user.username:
-              username = user.username
-          else:
-              username = ""
-          if user.first_name:
-              first_name = user.first_name
-          else:
-              first_name = ""
-          if user.last_name:
-              last_name = user.last_name
-          else:
-              last_name = ""
-          name = (first_name + ' ' + last_name).strip()
-          writer.writerow([username, user.id, user.access_hash,
-                          name, target_group.title, target_group.id])
-  print('Members scraped successfully.')
+  if(g_index) == 'x' or not g_index:
+      break
+  elif(g_index) == 'all':
+      print_group(groups)
+  else:
+      target_group = groups[int(g_index)]
+      print_group(target_group)
